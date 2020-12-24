96b2421d05a6335504563aa5cb1d274f8451f9ee
Tamme Schichler <tamme@schichler.dev>
2020-12-24 12:51:21 +0100

    Fixed warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![doc(html_root_url = "https://docs.rs/quote-miner/0.0.1")]
 #![warn(clippy::pedantic)]
+#![allow(clippy::filter_map)]
+#![allow(clippy::map_unwrap_or)]
 //TODO:
 #![allow(dead_code)]
 
